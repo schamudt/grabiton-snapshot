37c5c920b12955e2d13c2c7f59d30a03f4c1ec35
schamudt <schamudt@gmail.com>
2025-10-15 13:09:08 +0200

    Delete grabiton directory

--- grabiton/fragments/artists.php
+++
@@ -1,4 +0,0 @@
-<section class="gio-section">
-  <h1 class="gio-h1">Künstler*innen</h1>
-  <p>Demnächst: Künstlerliste, Suche …</p>
-</section>
--- grabiton/includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'db5018791850.hosting-data.io'); // aus dem Panel
-define('DB_NAME', 'dbs14849285');                   // sehr wahrscheinlich so! (dbs…, NICHT dbu…)
-define('DB_USER', 'dbu5635505');                   // exakt wie im Panel
-define('DB_PASS', 'music_4_the_masses_Brasilia_5647');          // eben neu gesetzt
-define('BASE_URL', 'https://www.grabiton.com');
